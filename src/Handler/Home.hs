1704c1ad8ce332b1b0769ce47dc56a71837101bf
Ubuntu <ubuntu@ip-172-31-33-160.us-east-2.compute.internal>
2019-10-31 00:02:20 +0000

    alterando

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,6 +10,14 @@ import Import
 -- import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+getPage1R :: Handler Html
+getPage1R = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(juliusFile "templates/page1.julius")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        $(whamletFile "templates/page1.hamlet")
+
 getHomeR :: Handler Html
 getHomeR = do
     defaultLayout $ do
@@ -24,7 +32,7 @@ getHomeR = do
         |]
         toWidgetHead [lucius|
             h1 {
-                color: red;
+                color: green;
             }
         |]
         [whamlet|
