00ef02c7ab50c6fe02e82f55a2feb207184851a7
Ubuntu <ubuntu@ip-172-31-33-160.us-east-2.compute.internal>
2019-10-31 00:09:10 +0000

    alterando

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,12 +10,18 @@ import Import
 -- import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+getPage2R :: Handler Html
+getPage2R = do
+    defaultLayout $ do
+        addStylesheet (StaticR css_bootstrap_css)
+        $(whamletFile "templates/page2.hamlet")
+
 getPage1R :: Handler Html
 getPage1R = do
     defaultLayout $ do
         addStylesheet (StaticR css_bootstrap_css)
-        toWidgetHead $(juliusFile "templates/page1.julius")
-        toWidgetHead $(luciusFile "templates/page1.lucius")
+        toWidgetHead $(julius "templates/page1.julius")
+        toWidgetHead $(lucius "templates/page1.lucius")
         $(whamletFile "templates/page1.hamlet")
 
 getHomeR :: Handler Html
@@ -34,11 +40,24 @@ getHomeR = do
             h1 {
                 color: green;
             }
+            
+            ul {
+                display: inline;
+                list-style: none;
+            }
         |]
         [whamlet|
             <div>
                 <h1>
                     OLA MUNDO
+            <ul>
+                <li>
+                    <a href=@{Page1R}>
+                        Pagina 1
+                <li>
+                    <a href=@{Page2R}>
+                        Pagina 2
+                        
             <img src=@{StaticR citeg_png}>
             <button class="btn btn-danger" onclick="ola()">
                 OK
